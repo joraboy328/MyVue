de9e4011674ab9cb2ebe060a6d68143fe6048ee6
Leon Liu / 刘刚 <gang.liu@dic.com.cn>
2019-07-17 18:50:48 +0800

    commit

---
+++ Vue实战/可排序表格组件/table.js
@@ -0,0 +1,17 @@
+Vue.component('vTable',{
+    props:{
+        columns:{
+            type:Array,
+            default:function() {
+                return [];
+            }
+        }
+    },
+    data:{
+        type:Array,
+        default:function() {
+            return [];
+        }
+
+    }
+});
