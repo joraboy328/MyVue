5279f6e9416fb6cf4c20c2aaa754735f06c4e927
Leon <gang.liu@dic.com.cn>
2019-07-17 21:27:28 +0800

    commit

--- Vue实战/可排序表格组件/index.js
+++ Vue实战/可排序表格组件/index.js
@@ -1,3 +1,59 @@
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        columns:[{
+            title:'姓名',
+            key:'name'
+        },
+        {
+            title:'年龄',
+            key:'age',
+            sortable:true
+        },
+        {
+            title:'出生日期',
+            key:'birthday',
+            sortable:true
+        },
+        {
+            title:'地址',
+            key:'address'
+        }],
+        data:[
+            {
+                name:'王小明',
+                age:18,
+                birthday:'1999-02-21',
+                address:'Beijin'
+            },
+            {
+                name:'张小刚',
+                age:25,
+                birthday:'1992-01-23',
+                address:'Jiangsu Nantong'
+            },
+            {
+                name:'李小红',
+                age:30,
+                birthday:'1987-11-10',
+                address:'Shanghai'
+            },
+            {
+                name:'周小伟',
+                age:26,
+                birthday:'1991-10-10',
+                address:'Suzhou'
+            },
+        ]
+    },
+    methods:{
+        handleAddData:function() {
+            this.data.push({
+                name:'刘小天',
+                age:19,
+                birthday:'1998-05-30',
+                address:'北京市东城区东直门'
+            });
+        }
+    }
 });
