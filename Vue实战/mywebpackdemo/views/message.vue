9e0ea309237e70bfd12c508391055dde95c436d3
Leon Liu / 刘刚 <gang.liu@dic.com.cn>
2019-07-24 07:43:01 +0800

    commit

---
+++ Vue实战/mywebpackdemo/views/message.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ message }}
+        <button @click="handleClick">Change word</button>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello World.'
+            };
+        },
+        methods:{
+            handleClick() {
+                this.message = 'Hello Vue.';
+            }
+        }
+    }
+</script>
