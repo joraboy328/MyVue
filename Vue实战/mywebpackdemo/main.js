d2bcfb16b20140331a7021424f4ad387ada3520a
Leon <gang.liu@dic.com.cn>
2019-07-24 21:06:41 +0800

    commit

--- Vue实战/mywebpackdemo/main.js
+++ Vue实战/mywebpackdemo/main.js
@@ -8,29 +8,48 @@ import App from './app.vue';
 Vue.use(VueRouter);
 
 
-const routes = [
+// 路由配置
+const Routers = [
     {
-        path : '/index',
-        component:(resolve) => require(['./views/index.vue'],resolve)
+        path: '/index',
+        meta: {
+            title: '首页'
+        },
+        component: (resolve) => require(['./views/index.vue'], resolve)
     },
     {
-        path : '/about',
-        component:(resolve) => require(['./views/about.vue'],resolve)
+        path: '/about',
+        meta: {
+            title: '关于'
+        },
+        component: (resolve) => require(['./views/about.vue'], resolve)
     },
+    
     {
-        path:'*',
-        redirect:'/index'
+        path: '*',
+        redirect: '/index'
     }
 ];
 const RouterConfig = {
-    mode:'history',
-    routes:routes
+    // 使用 HTML5 的 History 路由模式
+    mode: 'history',
+    routes: Routers
 };
-
 const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    window.document.title = to.meta.title;
+    next();
+});
+
+router.afterEach((to, from, next) => {
+    window.scrollTo(0, 0);
+});
+
 new Vue({
-    el:'#app',
-    router:router,
-    store:store,
-    render:h=>h(App)
+    el: '#app',
+    router: router,
+    render: h => {
+        return h(App)
+    }
 });
