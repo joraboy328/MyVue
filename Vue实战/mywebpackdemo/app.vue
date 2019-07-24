2aedb88610dabdb8216e25e3dd8814fe0eaccd12
Leon Liu / 刘刚 <gang.liu@dic.com.cn>
2019-07-24 09:18:45 +0800

    COMMIT

--- Vue实战/mywebpackdemo/app.vue
+++ Vue实战/mywebpackdemo/app.vue
@@ -1,37 +1,11 @@
 <template>
     <div>
-        <!--
-        <v-title title="Vue Component"></v-title>
-        <v-button @click="handleClick">Click Me</v-button>
-        <p>
-            <img src="./images/image.png" style="width:200px">
-        </p>
-      -->
       <router-view></router-view>
     </div>
 </template>
 <script>
-/*
-  import vTitle from './title.vue';
-  import vButton from './button.vue';
-
     export default {
-        components:{
-            vTitle,
-            vButton
-        },
-        methods:{
-            handleClick(e){
-
-                console.log(e);
-            }
-
         
-        }
     }
-*/
-export default {
-    
-}
 </script>
 
--- Vue实战/mywebpackdemo/main.js
+++ Vue实战/mywebpackdemo/main.js
@@ -1,22 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './app.vue';
-import index from './views/index.vue';
-import about from './views/about.vue';
+
 
 
 
 Vue.use(VueRouter);
-//Vue.use(Vuex);
-const routes = [
-    {
-        path:'/index',component:index
-    },
-    {
-        path:'/about',component:about
-    }
-]
-/*
+
+
 const routes = [
     {
         path : '/index',
@@ -30,24 +21,12 @@ const routes = [
         path:'*',
         redirect:'/index'
     }
-]*/
-/*
-const store = new Vuex.Store({
-    state:{
-        count:0
-    }
-});
-*/
+];
 const RouterConfig = {
     mode:'history',
     routes:routes
 };
-/*
-const router = new VueRouter({
-    routes:routes
-});
-export default router;
-*/
+
 const router = new VueRouter(RouterConfig);
 new Vue({
     el:'#app',
