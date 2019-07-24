47d92e7958b0408f8391bb81fe0917ab098a644a
Leon Liu / 刘刚 <gang.liu@dic.com.cn>
2019-07-24 10:56:16 +0800

    commit

--- Vue实战/mywebpackdemo/main.js
+++ Vue实战/mywebpackdemo/main.js
@@ -7,8 +7,8 @@ import App from './app.vue';
 
 Vue.use(VueRouter);
 
-
-const routes = [
+/*
+const Routers = [
     {
         path : '/index',
         component:(resolve) => require(['./views/index.vue'],resolve)
@@ -21,16 +21,51 @@ const routes = [
         path:'*',
         redirect:'/index'
     }
+];*/
+
+const Routers = [
+    {
+        path: '/index',
+        meta: {
+            title: '首页'
+        },
+        component: (resolve) => require(['./views/index.vue'], resolve)
+    },
+    {
+        path: '/about',
+        meta: {
+            title: '关于'
+        },
+        component: (resolve) => require(['./views/about.vue'], resolve)
+    },
+    {
+        path: '*',
+        redirect: '/index'
+    }
 ];
+
+
 const RouterConfig = {
     mode:'history',
-    routes:routes
+    routes:Routers
 };
 
 const router = new VueRouter(RouterConfig);
+
 new Vue({
     el:'#app',
     router:router,
-    store:store,
-    render:h=>h(App)
+    render:h=>{
+        return h(App)
+    }
 });
+
+/*
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => {
+        return h(App)
+    }
+});
+*/
